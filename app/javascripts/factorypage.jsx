ca09ee96804196c1d1c73e7919fb4db6d07edeb0
Simon de la Rouviere <simon@delarouviere.com>
2015-12-17 18:37:01 +0200

    Moved to older libs due to reflux-tx added.

--- app/javascripts/factorypage.jsx
+++ app/javascripts/factorypage.jsx
@@ -10,30 +10,13 @@ var FactoryPage = React.createClass({
   render: function() {
     return (
       <div>
-        Enter the amount, the creator will receive: <br />
-        <br />
-        <input type="text" value={this.state.value} placeholder="eg 10000" onChange={this.handleChange}/>
-        <button onClick={this.executeFunction}>Create Token</button>
+        <TXComponent filter={{txType: "token_creation"}}>
+            <CreateTokenForm />
+        </TXComponent>
         ---
         (show what tokens current selected address has created).
       </div>
     );
-  },
-  executeFunction: function() {
-    //check if number. (or force that in the react form).
-    //do loader gif/reflux store?
-    //replace address with light-wallet/uPort.
-
-    //for now, deployed separately, until factory is online.
-    //thus users must remember their addresses (for now).
-    var st_ctr = null;
-    var comp = this;
-    Standard_Token.new(this.state.value, {from: web3.eth.accounts[0]}).then(function(ctr) {
-        st_ctr = ctr;
-        return ctr.balanceOf.call(web3.eth.accounts[0]);
-    }).then(function (result) {
-        comp.props.history.pushState(null,'/token/'+st_ctr.address);
-    });
   }
 });
 
--- app/javascripts/main.jsx
+++ app/javascripts/main.jsx
@@ -1,9 +1,12 @@
-// This makes ReactRouter more consumable
-// Copying code from dapp-store & inflekt. Will figure out nuances later.
+//currently uses modified reflux-tx & modified ethersim to work.
+//also moved to react 0.13 (history 1.13 & removed reactdom) for reflux-tx to work.
 window.Route = ReactRouter.Route;
 window.Router = ReactRouter.Router;
 window.IndexRoute = ReactRouter.IndexRoute;
 window.Link = ReactRouter.Link;
+window.TXActions = refluxTX.TXActions;
+window.TXComponent = refluxTX.TXComponent;
+TXActions.connect({provider: 'http://localhost:8545', confirmCount: 5, bufferSize: 5})
 
 //remove _k thing from URLS (removing queryKey)
 window.histor = History.createHashHistory({
@@ -25,7 +28,8 @@ window.onload = function() {
   // check if RPC is online. Why though?
   web3.eth.getCoinbase(function(error, coinbase) {
     window.MainRouter = Router;
-    ReactDOM.render((
+    //ReactDOM.render((
+    React.render((
       <Router history={histor}>
         <Route path="/" component={App}>
           <IndexRoute component={FrontPage} />
