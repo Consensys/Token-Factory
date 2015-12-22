70902d5a943c8580288544ec4e56d15ca9b7a8c8
Simon de la Rouviere <simon@delarouviere.com>
2015-12-22 10:37:55 +0200

    Added total supple to token page.

--- app/javascripts/tokenpage.jsx
+++ app/javascripts/tokenpage.jsx
@@ -4,35 +4,41 @@ var TokenPage = React.createClass({
     return {
       address: '',
       transferAmount: 0,
-      web3_contract: '',
-      pudding_contract: '',
+      web3_token: '',
+      pudding_token: '',
     };
   },
   componentDidMount: function() {
     this.setState({address: this.props.params.address});
-    var web3_contract = web3.eth.contract(Standard_Token.abi).at(this.props.params.address); //for reflux-tx
-    var pudding_contract = Standard_Token.at(this.props.params.address); //for promises
-    this.setState({web3_contract: web3_contract});
-    this.setState({pudding_contract: pudding_contract});
+    var web3_token = web3.eth.contract(Standard_Token.abi).at(this.props.params.address); //for reflux-tx
+    var pudding_token = Standard_Token.at(this.props.params.address); //for promises
+    this.setState({web3_token: web3_token});
+    this.setState({pudding_token: pudding_token});
+    var that = this;
+    pudding_token.totalSupply.call({from: web3.eth.accounts[0]}).then(function (result) {
+      //console.log(result);
+      that.setState({totalSupply: result.c[0]});
+    });
   },
   render: function() {
     //return error if not actual token system.
     return (
       <div>
         Interacting with token at address: {this.state.address}. <br />
+      Total Supply is: {this.state.totalSupply}.
         <br />
         Basic Functions: <br />
         <br />
         <TXComponent filter={{txType: "transfer"}}>
-          <TransferForm pudding_token = {this.state.pudding_contract} web3_token = {this.state.web3_contract} />
+          <TransferForm pudding_token = {this.state.pudding_token} web3_token = {this.state.web3_token} />
         </TXComponent> <br />
 
         <TXComponent filter={{txType: "approve"}}>
-          <ApproveForm pudding_token = {this.state.pudding_contract} web3_token = {this.state.web3_contract} />
+          <ApproveForm pudding_token = {this.state.pudding_token} web3_token = {this.state.web3_token} />
         </TXComponent> <br />
 
         <UnapproveForm />
-        <BalanceOfForm pudding_token = {this.state.pudding_contract} web3_token = {this.state.web3_contract} /> <br />
+        <BalanceOfForm pudding_token = {this.state.pudding_token} web3_token = {this.state.web3_token} /> <br />
 
         Other Functions: <br />
         (TransferFrom)
