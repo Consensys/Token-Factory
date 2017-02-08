bb373bb0ef154ad3dea49a4e244b4006fd966b3f
Bill Gleim <bgleim@gmail.com>
2017-02-08 14:55:59 -0800

    Successful migration to infura ropsten

--- truffle.js
+++ truffle.js
@@ -1,9 +1,20 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "<replace with twelve-word mnemonic phrase ... >"; // 12 word mnemonic 
+var provider = new HDWalletProvider(mnemonic, "https://ropsten.infura.io/<replace with infura access token>");
+
+// display address to screen to fund for further operations
+console.log(provider.getAddress());
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: provider,
+      network_id: 3
     }
   }
 };
