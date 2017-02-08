34059b71987ab5f76fc73b8f73f1d4a72f88a431
Bill Gleim <bgleim@gmail.com>
2017-02-08 09:33:23 -0800

    Upgrade migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var HumanStandardTokenFactory = artifacts.require("./HumanStandardTokenFactory.sol");
+var HumanStandardToken = artifacts.require("./HumanStandardToken.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(HumanStandardTokenFactory);
+  deployer.deploy(HumanStandardToken);
 };
